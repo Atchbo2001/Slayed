7950801d4747bb54f30acee9ec5113db313c7e58
James <james.auble@gmail.com>
2021-08-04 16:41:12 +0900

    worked out add, get, and clear cart of the cart Alpine store

--- src/alpine/stores/cart.js
+++ src/alpine/stores/cart.js
@@ -1,12 +1,57 @@
 export default {
   name: 'cart',
-  store () {
+  store() {
     return {
       counter: 0,
       items: [],
-      test() {
-        this.counter = this.counter + 1
-        console.log( this.counter );
+      add(data) {
+        fetch('/cart/add.js', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+          .then((response) => response.json())
+          .then((addedItemData) => {
+            // const itemExists = this.items.some(item => item.key != addedItemData.key && item.variant_id == addedItemData.variant_id)
+
+            this.items[addedItemData.variant_id] = addedItemData
+          })
+          .catch((error) => {
+            console.error('Error:', error)
+          })
+      },
+      async get() {
+        let response = await fetch('/cart.js', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+
+        let data = await response.json()
+
+        return data
+      },
+      update() {
+      },
+      clear() {
+        fetch('/cart/clear.js', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            if (data.item_count === 0) {
+              this.items = []
+            }
+          })
+          .catch((error) => {
+            console.error('Error:', error)
+          })
       }
     }
   }
